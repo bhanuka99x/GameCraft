03f6fbe212e526cf46b486f787744017ab20d46b
Bhanuka Gihan <51521654+Bhanuka99x@users.noreply.github.com>
2024-11-03 14:16:44 +0530

    fix nav bar issue

--- GameCraft/Store.php
+++ GameCraft/Store.php
@@ -4,6 +4,7 @@ include_once 'connection.php';
 $sql="SELECT*FROM product";
 $all_games = $conn->query($sql);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,43 +16,7 @@ $all_games = $conn->query($sql);
     <script defer src="./JS/store.js"></script>
 </head>
 <body>
-
-<!--Side navbar-->
-  <nav>
-    <button type="button" id="toggle-btn">
-      <i class="fa fa-bars"></i>
-    </button>
-    <span>GameCraft</span>
-    <ul class="sidebar-menu">
-      <li><a href="#"><i class='bx bx-home-alt' ></i>Home</a></li>
-      <li><a href="#"><i class='bx bx-store-alt' ></i>Store</a></li>
-      <li><a href="#"><i class='bx bx-library' ></i>Library</a></li>
-      <li><a href="#"><i class='bx bx-joystick' ></i>Cart</a></li>
-      <li id="bgModeBtn"><a href="#"><i id="bgModeIcon" class='bx bx-sun'></i>Mode</a></li>
-    </ul>
-  </nav>
-
-  <!--top navbar-->
- 
-
-<div id="topnav-bar">   
-  <div class="logo1">
   
-
- </div> 
-  
-
-  <div class="topnav-search">
-       <input type="text" id="site-search" name="search" placeholder="Search.." value ="<?php if(isset($_GET['search'])){echo $GET_['search'];} ?>">
-       <button type="submit" id="btnsearch">Search</button>
-  </div>
-
-  
-     <div class="topnav-user">
-       <button id="b3"><i class='bx bx-user'></i></button>
-     </div>
-</div>
-
 <main>
 <?php
    while($row = mysqli_fetch_assoc($all_games)){
@@ -77,14 +42,10 @@ $all_games = $conn->query($sql);
 ?>
 </main>
 
-
-
-
-
-
-
-
-
+<?php
+  include_once 'second-nav.php'
+  ?>
+  
 <?php
 include_once 'footer.php';
 ?>
