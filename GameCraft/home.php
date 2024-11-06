fbdba4884d7377c62d37d7e2275442fc62e3a823
Bhanuka Gihan <51521654+Bhanuka99x@users.noreply.github.com>
2024-11-06 18:48:16 +0530

    develop home page

--- GameCraft/home.php
+++ GameCraft/home.php
@@ -1,11 +1,144 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
+    <title>Document</title>
+    <link rel="stylesheet" href="./CSS/home.css">
 </head>
 <body>
-    <h1> hello gamers</h1>
+
+<!-- carousel -->
+    <div class="carousel">
+        <!-- list item -->
+        <div class="list">
+            <div class="item">
+                <img src="./Images/Alan wake 2 1080p.jpg">
+                <div class="content">
+                    <div class="author">TEST</div>
+                    <div class="title">DESIGN TEST</div>
+                    <div class="topic">TEST NAME</div>
+                    <div class="des">
+                        <!-- lorem 50 -->
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
+                    </div>
+                    <div class="buttons">
+                        <button>TEST BUTTON</button>
+                        <button>TEST BUTTON</button>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <img src="./Images/Farcry 6 1080p.jpg">
+                <div class="content">
+                    <div class="author">TEST</div>
+                    <div class="title">DESIGN TEST</div>
+                    <div class="topic">TEST NAME</div>
+                    <div class="des">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
+                    </div>
+                    <div class="buttons">
+                        <button>TEST BUTTON</button>
+                        <button>TEST BUTTON</button>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <img src="./Images/god of war 1080p.jpg">
+                <div class="content">
+                    <div class="author">TEST</div>
+                    <div class="title">DESIGN TEST</div>
+                    <div class="topic">TEST NAME</div>
+                    <div class="des">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
+                    </div>
+                    <div class="buttons">
+                        <button>TEST BUTTON</button>
+                        <button>TEST BUTTON</button>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <img src="./Images/last of us 1080p.jpg">
+                <div class="content">
+                    <div class="author">TEST</div>
+                    <div class="title">DESIGN TEST</div>
+                    <div class="topic">TEST NAME</div>
+                    <div class="des">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
+                    </div>
+                    <div class="buttons">
+                        <button>TEST BUTTON</button>
+                        <button>TEST BUTTON</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- list thumnail -->
+        <div class="thumbnail">
+            <div class="item">
+                <img src="./Images/Alan wake 2 1080p.jpg">
+                <div class="content">
+                    <div class="title">
+                        Name Slider
+                    </div>
+                    <div class="description">
+                        Description
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <img src="./Images/Farcry 6 1080p.jpg">
+                <div class="content">
+                    <div class="title">
+                        Name Slider
+                    </div>
+                    <div class="description">
+                        Description
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <img src="./Images/god of war 1080p.jpg">
+                <div class="content">
+                    <div class="title">
+                        Name Slider
+                    </div>
+                    <div class="description">
+                        Description
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <img src="./Images/last of us 1080p.jpg">
+                <div class="content">
+                    <div class="title">
+                        Name Slider
+                    </div>
+                    <div class="description">
+                        Description
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- next prev -->
+
+        <div class="arrows">
+            <button id="prev"><</button>
+            <button id="next">></button>
+        </div>
+        <!-- time running -->
+        <div class="time"></div>
+    </div>
+    
+
+    <script src="./JS/home.js"></script>
+
+    <?php
+    include_once 'second-nav.php';
+    include_once 'footer.php';
+
+    ?>
 </body>
 </html>
