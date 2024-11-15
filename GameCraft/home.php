c5c5c784e50a9f4b6d78a732858d708db3826780
Nimesha Bandara <theekshanab3@gmail.com>
2024-11-15 23:25:55 +0530

    develop whitelist

--- GameCraft/home.php
+++ GameCraft/home.php
@@ -6,28 +6,24 @@ $carouselItems = $result->fetch_all(MYSQLI_ASSOC);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GameCraft</title>
     <link rel="stylesheet" href="./CSS/home.css">
 </head>
+
 <body>
 
-<header>
+    <header>
         <nav class="nav-left">
             <a href="#" class="logo">Gamecraft</a>
             <a href="home.php" class="nav-item">Home</a>
             <a href="store.php" class="nav-item">Store</a>
             <a href="whitelist.php" class="nav-item">whitelist</a>
             <a href="library.php" class="nav-item">Library</a>
-            <?php
-      
-      $select_rows = mysqli_query($conn, "SELECT * FROM cart") or die('query failed');
-      $row_count = mysqli_num_rows($select_rows);
-
-      ?>
-        <a href="cart.php" class="nav-item">Cart<span><?php echo $row_count; ?></span></a>
+            <a href="cart.php" class="nav-item">Cart</a>
         </nav>
         <nav class="nav-right">
             <a href="#" class="l-btn">Login</a>
@@ -36,11 +32,11 @@ $carouselItems = $result->fetch_all(MYSQLI_ASSOC);
     </header>
 
 
-<!-- carousel -->
-<div class="carousel">
-    <!-- list item -->
-    <div class="list">
-        <?php foreach ($carouselItems as $item): ?>
+    <!-- carousel -->
+    <div class="carousel">
+        <!-- list item -->
+        <div class="list">
+            <?php foreach ($carouselItems as $item): ?>
             <div class="item">
                 <img src="./Images/<?php echo $item['himage']; ?>" alt="">
                 <div class="content">
@@ -51,32 +47,34 @@ $carouselItems = $result->fetch_all(MYSQLI_ASSOC);
                     </div>
                 </div>
             </div>
-        <?php endforeach; ?>
-    </div>
-    <!-- list thumbnail -->
-    <div class="thumbnail">
-        <?php foreach ($carouselItems as $item): ?>
+            <?php endforeach; ?>
+        </div>
+        <!-- list thumbnail -->
+        <div class="thumbnail">
+            <?php foreach ($carouselItems as $item): ?>
             <div class="item">
                 <img src="./Images/<?php echo $item['himage']; ?>" alt="<?php echo $item['hdes']; ?>">
                 <div class="content">
                     <div class="title"><?php echo $item['hname']; ?></div>
                 </div>
             </div>
-        <?php endforeach; ?>
+            <?php endforeach; ?>
+        </div>
+        <!-- next prev -->
+        <div class="arrows">
+            <button id="prev">
+                << /button>
+                    <button id="next">></button>
+        </div>
+        <!-- time running -->
+        <div class="time"></div>
     </div>
-    <!-- next prev -->
-    <div class="arrows">
-        <button id="prev"><</button>
-        <button id="next">></button>
-    </div>
-    <!-- time running -->
-    <div class="time"></div>
-</div>
 
-<script src="./JS/home.js"></script>
+    <script src="./JS/home.js"></script>
 
-<?php
+    <?php
 include_once 'footer.php';
 ?>
 </body>
+
 </html>
